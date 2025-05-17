e68d262b35ddaf0df76281ae04ca35656afb2645
Alma <isakvent@outlook.com>
2025-05-17 22:44:13 +0200

    import images and .json files for all other health keys

--- adventist-uppsala-halsa/src/assets/images/MapHealthKeyImages.ts
+++ adventist-uppsala-halsa/src/assets/images/MapHealthKeyImages.ts
@@ -1,9 +1,21 @@
 // Script that automatically locates images based by the id name, since .json files cannot store images
 import WaterImage from "./unsplash-nicolas-ruiz-water.jpg";
 import TemperanceImage from "./unsplash-tingey-injury-law-firm-temperance.jpg";
+import ExersiceImage from "./unsplash-martin-dalsgaard-exersice.jpg";
+import SunshineImage from "./unsplash-melissa-askew-sunshine.jpg";
+import RestImage from "./unsplash-shahovskaya-rest.jpg";
+import TrustInGod from "./unsplash-ioana-ye-trust-in-god.jpg";
+import AirImage from "./unsplash-mauri-karlin-air.jpg";
+import NutritionImage from "./unsplash-victoria-shes-nutrition.jpg";
 
-// each record name MUST match the id from .json files
+// each record name MUST match the id from .json files (use camelCase)
 export const MapHealthKeyImages: Record<string, string[]> = {
     "water" : [WaterImage],
-    "temperance" : [TemperanceImage]
+    "temperance" : [TemperanceImage],
+    "exersice" : [ExersiceImage],
+    "sunshine" : [SunshineImage],
+    "rest" : [RestImage],
+    "trustInGod" : [TrustInGod],
+    "air" : [AirImage],
+    "nutrition" : [NutritionImage]
 };
--- adventist-uppsala-halsa/src/pages/EightHealthKeys/EightHealthKeysWrapper.tsx
+++ adventist-uppsala-halsa/src/pages/EightHealthKeys/EightHealthKeysWrapper.tsx
@@ -6,12 +6,24 @@ import { useViewPortContext } from "../../contexts/useViewportContext";
 import { useState } from "react";
 import waterContent from "../../content/healthKeys/water.json";
 import temperanceContent from "../../content/healthKeys/temperance.json";
+import exersiceContent from "../../content/healthKeys/exersice.json";
+import sunshineContent from "../../content/healthKeys/sunshine.json";
+import restContent from "../../content/healthKeys/rest.json";
+import trustInGodContent from "../../content/healthKeys/trust-in-god.json";
+import airContent from "../../content/healthKeys/air.json";
+import nutritionContent from "../../content/healthKeys/nutrition.json";
 
 const EightHealthKeysWrapper = () => {
     const { viewportWidth } = useViewPortContext();
     const VIEWPORT_BREAKPOINT = 768;
     const [waterExpanded, setWaterExpanded] = useState<boolean>(false); // "Vatten"
     const [temperanceExpanded, setTemperanceExpanded] = useState<boolean>(false); // "Avhållsamhet"
+    const [exersiceExpanded, setExersiceExpanded] = useState<boolean>(false); // "Rörelse"
+    const [sunshineExpanded, setSunshineExpanded] = useState<boolean>(false); // "Solsken"
+    const [restExpanded, setRestExpanded] = useState<boolean>(false); // "Återhämtning"
+    const [trustInGodExpanded, setTrustInGodExpanded] = useState<boolean>(false); // "Gudsförtröstan"
+    const [airExpanded, setAirExpanded] = useState<boolean>(false); // "O2 syre"
+    const [nutritionExpanded, setNutritionExpanded] = useState<boolean>(false); // "Diet"
 
     return (
         <div className={Styles.EightHealthKeysWrapperComponent}>
@@ -41,6 +53,84 @@ const EightHealthKeysWrapper = () => {
                     contentData={temperanceContent}
                     isExpanded={temperanceExpanded}
                     setIsExpanded={setTemperanceExpanded}
+                />
+            }
+            {viewportWidth < VIEWPORT_BREAKPOINT ?
+                <PhoneHealthKeyCard
+                    contentData={exersiceContent}
+                    isExpanded={exersiceExpanded}
+                    setIsExpanded={setExersiceExpanded}
+                />
+                :
+                <LargeHealthKeyCard
+                    contentData={exersiceContent}
+                    isExpanded={exersiceExpanded}
+                    setIsExpanded={setExersiceExpanded}
+                />
+            }
+            {viewportWidth < VIEWPORT_BREAKPOINT ?
+                <PhoneHealthKeyCard
+                    contentData={sunshineContent}
+                    isExpanded={sunshineExpanded}
+                    setIsExpanded={setSunshineExpanded}
+                />
+                :
+                <LargeHealthKeyCard
+                    contentData={sunshineContent}
+                    isExpanded={sunshineExpanded}
+                    setIsExpanded={setSunshineExpanded}
+                />
+            }
+            {viewportWidth < VIEWPORT_BREAKPOINT ?
+                <PhoneHealthKeyCard
+                    contentData={restContent}
+                    isExpanded={restExpanded}
+                    setIsExpanded={setRestExpanded}
+                />
+                :
+                <LargeHealthKeyCard
+                    contentData={restContent}
+                    isExpanded={restExpanded}
+                    setIsExpanded={setRestExpanded}
+                />
+            }
+            {viewportWidth < VIEWPORT_BREAKPOINT ?
+                <PhoneHealthKeyCard
+                    contentData={trustInGodContent}
+                    isExpanded={trustInGodExpanded}
+                    setIsExpanded={setTrustInGodExpanded}
+                />
+                :
+                <LargeHealthKeyCard
+                    contentData={trustInGodContent}
+                    isExpanded={trustInGodExpanded}
+                    setIsExpanded={setTrustInGodExpanded}
+                />
+            }
+            {viewportWidth < VIEWPORT_BREAKPOINT ?
+                <PhoneHealthKeyCard
+                    contentData={airContent}
+                    isExpanded={airExpanded}
+                    setIsExpanded={setAirExpanded}
+                />
+                :
+                <LargeHealthKeyCard
+                    contentData={airContent}
+                    isExpanded={airExpanded}
+                    setIsExpanded={setAirExpanded}
+                />
+            }
+            {viewportWidth < VIEWPORT_BREAKPOINT ?
+                <PhoneHealthKeyCard
+                    contentData={nutritionContent}
+                    isExpanded={nutritionExpanded}
+                    setIsExpanded={setNutritionExpanded}
+                />
+                :
+                <LargeHealthKeyCard
+                    contentData={nutritionContent}
+                    isExpanded={nutritionExpanded}
+                    setIsExpanded={setNutritionExpanded}
                 />
             }
 
