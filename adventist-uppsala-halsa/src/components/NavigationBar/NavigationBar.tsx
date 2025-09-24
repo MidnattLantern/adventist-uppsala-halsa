17220310c78a2484db62cb05454b277bd6331dcb
Alma <isakvent@outlook.com>
2025-09-24 21:50:49 +0200

    make the new navigation bar appropriate for large screens

--- adventist-uppsala-halsa/src/components/NavigationBar/NavigationBar.tsx
+++ adventist-uppsala-halsa/src/components/NavigationBar/NavigationBar.tsx
@@ -6,17 +6,23 @@ import {ReactComponent as CloseIcon} from "../../assets/vector-icons/close-icon.
 import {ReactComponent as LinkIcon} from "../../assets/vector-icons/generic-star-icon.svg";
 import {ReactComponent as HomeIcon} from "../../assets/vector-icons/home-icon.svg";
 import {ReactComponent as ExternalLinkIcon} from "../../assets/vector-icons/external-link-icon.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const BREAKPOINT_SHORT_TITLE = 768
+const BREAKPOINT_WIDTH = 768;
 
 const NavigationBar = () => {
     const { viewportWidth } = useViewPortContext();
     const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
+    useEffect(() => {
+        if (viewportWidth > BREAKPOINT_WIDTH) {
+            setIsExpanded(false);
+        }
+    }, [viewportWidth])
+
     const BurgerExpand = () => {
         return (
-            <button onClick={() => {setIsExpanded(!isExpanded)}} className={Styles.BurgerCloseExpandButton}>
+            <button onClick={() => {setIsExpanded(!isExpanded)}} className={`${Styles.BurgerCloseExpandButton} ${isExpanded && Styles.BurgerCloseExpanded}`}>
                 {isExpanded ?
                 <CloseIcon className={Styles.BurgerCloseIconColor}/>
                 :
@@ -30,44 +36,44 @@ const NavigationBar = () => {
         return (
             <div className={`${Styles.LinksView} ${isExpanded && Styles.LinksViewExpanded}`}>
                 <Link to="/" onClick={() => {setIsExpanded(false)}} className={Styles.HomeLink}>
-                    <HomeIcon className={Styles.LinkIconSize}/> {"Adventkyrkan Hälsa"}
+                    <HomeIcon className={Styles.LinkIconSize}/> <p>{"Adventkyrkan Hälsa"}</p>
                 </Link>
                 <a href="https://uppsala.adventkyrka.se/" target="_blank" rel="noreferrer" className={Styles.Link}>
-                    <ExternalLinkIcon className={Styles.LinkIconSize}/> {"Adventistkyrkan Uppsala"}
+                    <ExternalLinkIcon className={Styles.LinkIconSize}/> <p>{"Adventistkyrkan Uppsala"}</p>
                 </a>
                 <hr className={Styles.LineBreak}/>
                 <Link to="/åtta-hälsonycklar" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"8 hälsonycklar"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"8 hälsonycklar"}</p>
                 </Link>
                 <Link to="/vilka-är-vi" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"Vilka är vi?"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"Vilka är vi?"}</p>
                 </Link>
                 <Link to="/förkylning-och-naturlig-behandling" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"Förkylning & naturlig behandling"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"Förkylning & naturlig behandling"}</p>
                 </Link>
                 <hr className={Styles.LineBreak}/>
                 <Link to="/missbruk" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"Missbruk"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"Missbruk"}</p>
                 </Link>
                 <Link to="/hälsa-för-nervsystemet" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"Hälsa för nervsystemet"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"Hälsa för nervsystemet"}</p>
                 </Link>
                 <Link to="/örter-till-människans-tjänst" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"Örter till människans tjänst"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"Örter till människans tjänst"}</p>
                 </Link>
                 <hr className={Styles.LineBreak}/>
                 <Link to="/första-hjälpen-kit" onClick={() => {setIsExpanded(false)}} className={Styles.Link}>
-                    <LinkIcon className={Styles.LinkIconSize}/> {"1:a hjälpen kit"}
+                    <LinkIcon className={Styles.LinkIconSize}/> <p>{"1:a hjälpen kit"}</p>
                 </Link>
             </div>
         )
     }
 
     return (
-        <div className={Styles.NavigationBarView}>
-            <BurgerExpand/>
+        <>
+            {viewportWidth < BREAKPOINT_WIDTH && <BurgerExpand/>}
             <LinksView/>
-        </div>
+        </>
     )
 };
 
